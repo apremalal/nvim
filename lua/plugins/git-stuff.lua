6ca59da5e01410f9d1db3a110be23691c99cc800
ap <anuruddha@artafinance.com>
2025-06-21 16:16:49 +0800

    fzf

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -7,6 +7,7 @@ return {
 			})
 			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
 			vim.keymap.set("n", "<leader>gb", ":Gitsigns blame<CR>")
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>")
 		end,
 	},
 	{
