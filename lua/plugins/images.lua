25c646d7f1e5664d4533a76d88560056576bc2c5
ap <anuruddha@artafinance.com>
2025-06-29 16:25:12 +0800

    image fixes for kitty

--- lua/plugins/images.lua
+++ lua/plugins/images.lua
@@ -1,10 +1,21 @@
 return {
-	"3rd/image.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	enabled = false,
-	config = function()
-		require("image").setup({
-			processor = "magick_cli",
-		})
-	end,
+	{
+		"3rd/image.nvim",
+		commit = "21909e3eb03bc738cce497f45602bf157b396672",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		build = false,
+		config = function()
+			require("image").setup({
+				backend = "kitty",
+				processor = "magick_cli",
+				integrations = {
+					markdown = {
+						enabled = true,
+						only_render_image_at_cursor = false,
+						download_remote_images = false,
+					},
+				},
+			})
+		end,
+	},
 }
